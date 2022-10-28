6725b9bed13d2ff58e106fef2e575cd37cc0a910
BLew10 <Brandonlewis.10@gmail.com>
2022-10-28 16:06:36 -0700

    initial commit

---
+++ React_Template/client/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import {
+  Routes,
+  Route,
+  Link,
+  useNavigate
+} from "react-router-dom"
+
+//installment codes:
+// npx create-react-app
+// npm install axios
+// npm install react-router-dom
+// npm init -y
+// npm install mongoose express
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <Routes>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ React_Template/client/src/components/TempComponent.jsx
@@ -0,0 +1,4 @@
+import React, { useState, useEffect }from 'react'
+import { useParams } from "react-router";
+import { useNavigate } from "react-router-dom"
+import axios from 'axios'
